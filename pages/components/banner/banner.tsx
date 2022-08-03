469612fe9e32ceff137e4f661513af3df0a36784
Ryan Webster <webstertrajano2003@gmail.com>
2022-08-02 22:31:27 -0400

    Responsive adjustments and new alert

--- pages/components/banner/banner.tsx
+++ pages/components/banner/banner.tsx
@@ -3,8 +3,8 @@ import styles from '../../../styles/Banner.module.css'
 export default function Banner() {
     return (
         <div className={styles.content}>
-            <div className={styles.container}>
-                <h1 className={styles.text}>The tech blog <br/> made for  you</h1>
+            <div className='container-md' style={{position: 'absolute', width: '50%'}}>
+                <h1 className='display-1 fw-normal'>The tech blog <br/> made for  you</h1>
                 
             </div>
             
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import type { NextPage } from 'next'
+import { useState } from 'react';
+import { Alert } from 'react-bootstrap';
 import Banner from './components/banner/banner';
 
 import Content from './components/content/content';
@@ -8,10 +10,19 @@ import NavBar from './components/navbar/navbar';
 
 const Index: NextPage = () => {
   
+  const [show, setShow] = useState(true);
+
     return (
       <>
         <main>
             <NavBar/>
+              <Alert show={show} variant="warning" onClose={() => setShow(false)} dismissible>
+              <Alert.Heading>Hello</Alert.Heading>
+              <p>
+                This site is under development, thank you for understanding.
+              </p>
+            </Alert>
+            
             <Banner/>
             <Content/>
             <Footer/>
