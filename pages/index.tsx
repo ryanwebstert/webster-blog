467ce9a8599d2fdaed948b1410d0703b0514369d
Ryan Webster <webstertrajano2003@gmail.com>
2022-10-02 00:15:11 -0400

    aviso desenvolvimento

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
-import { useState } from 'react';
-import { Alert } from 'react-bootstrap';
+import { DetailedHTMLProps, HTMLAttributes, ReactNode, RefObject, useState } from 'react';
+import { Alert, Button, Modal, ModalProps } from 'react-bootstrap';
+import { Omit, BsPrefixProps } from 'react-bootstrap/esm/helpers';
 import Banner from './components/banner/banner';
 
 import Content from './components/content/content';
@@ -8,20 +9,25 @@ import Footer from './components/footer/footer';
 import NavBar from './components/navbar/navbar';
 
 
+
+
+
+
+
 const Index: NextPage = () => {
   
-  const [show, setShow] = useState(Boolean);
+  const [show, setShow] = useState(true);
 
 
     return (
       <>
         <main>
             <NavBar/>
-              <Alert show={show} variant="warning" onClose={() => setShow(false)} dismissible>
-              <Alert.Heading>Hello</Alert.Heading>
-              <p>
-                This site is under development, thank you for understanding.
-              </p>
+            <Alert variant="warning" onClose={() => setShow(false)} >
+              <Alert.Heading>Olá</Alert.Heading>
+                <p>
+                  Este site está em desenvolvimento. Agradeço sua visita.
+                </p>
             </Alert>
             
             <Banner/>
